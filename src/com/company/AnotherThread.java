7d9b22bd6c648116468efae80df461aabfa8a953
Serhii Miroshnichenko <ragazzo@ukr.net>
2019-08-13 16:48:08 +0300

    add try

--- src/com/company/AnotherThread.java
+++ src/com/company/AnotherThread.java
@@ -7,5 +7,12 @@ public class AnotherThread extends Thread {
     public void run() {
         System.out.println(ANSI_BLUE + "Hello from " + currentThread().getName());
 
+        try {
+            Thread.sleep(3000);
+        }catch (InterruptedException e){
+            System.out.println(ANSI_BLUE + "Another thread woke me up");
+        }
+
+        System.out.println(ANSI_BLUE + "Three seconds have passed and I'm awake");
     }
 }
