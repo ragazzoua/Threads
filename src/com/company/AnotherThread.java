04ff8f56c7c6c148579c0be7d0ec65973597e7c9
Serhii Miroshnichenko <ragazzo@ukr.net>
2019-08-13 16:39:25 +0300

    start several threads

--- src/com/company/AnotherThread.java
+++ src/com/company/AnotherThread.java
@@ -5,7 +5,7 @@ import static com.company.ThreadColor.ANSI_BLUE;
 public class AnotherThread extends Thread {
     @Override
     public void run() {
-        System.out.println(ANSI_BLUE + "Hello from another thread");
+        System.out.println(ANSI_BLUE + "Hello from " + currentThread().getName());
 
     }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,6 +2,7 @@ package com.company;
 
 import static com.company.ThreadColor.ANSI_GREEN;
 import static com.company.ThreadColor.ANSI_PURPLE;
+import static com.company.ThreadColor.ANSI_RED;
 
 public class Main {
 
@@ -9,13 +10,23 @@ public class Main {
         System.out.println(ANSI_PURPLE + "Hello from the main thread.");
 
         Thread anotherThread = new AnotherThread();
+        anotherThread.setName("== Another Thread ==");
         anotherThread.start();
 
         new Thread(){
             public void run(){
-                System.out.println(ANSI_GREEN  + "hello from the anonymous class thread");
+                System.out.println(ANSI_GREEN  + "Hello from the anonymous class thread");
             }
         }.start();
+
+        Thread myRunnablethread = new Thread(new MyRunnable(){
+            @Override
+            public void run() {
+                System.out.println(ANSI_RED + "Hello from the anonymous class's implementatiom of run");
+            }
+        });
+
+        myRunnablethread.start();
 
         System.out.println(ANSI_PURPLE + "Hello again from main thread.");
     }
